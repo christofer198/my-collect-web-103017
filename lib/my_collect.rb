4fa48d7792031ad4d8d5da34cd8dd83bfab77923
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-10-15 01:42:33 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+def my_collect(array)
 
+  counter = 0
+  collection = []
+  while counter != array.length
+    yielding = yield(array[counter])
+    collection[counter] = yielding
+  end
+  return collection
+end
+    
 
